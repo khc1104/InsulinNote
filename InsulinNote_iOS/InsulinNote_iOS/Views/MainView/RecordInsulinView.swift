8fd36d3569a538c5dd1421d362d6a92fb315cf30
Heecheol_K <hck1104@naver.com>
2024-07-27 20:33:01 +0900

    인슐린 투여양 표시뷰 제장 중

--- InsulinNote_iOS/InsulinNote_iOS/Views/MainView/RecordInsulinView.swift
+++ InsulinNote_iOS/InsulinNote_iOS/Views/MainView/RecordInsulinView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct RecordInsulinView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Rectangle()
+                .foregroundStyle(.gray)
+            Text("인슐린")
+        }
     }
 }
 

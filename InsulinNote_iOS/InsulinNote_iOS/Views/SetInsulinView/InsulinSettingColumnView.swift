a77948c1deb0b1ed7a89c0e843eef6118ddf37b3
Heecheol_K <hck1104@naver.com>
2024-08-03 20:58:59 +0900

    인슐린 설정 뷰 제품 수 환산 버튼 추가 및 디자인 수정

--- InsulinNote_iOS/InsulinNote_iOS/Views/SetInsulinView/InsulinSettingColumnView.swift
+++ InsulinNote_iOS/InsulinNote_iOS/Views/SetInsulinView/InsulinSettingColumnView.swift
@@ -15,15 +15,45 @@ struct InsulinSettingColumnView: View {
     var removeButtonTapped: () -> ()
     
     var body: some View {
-        HStack{
-            VStack(alignment: .leading){
-                Text("제품명 : \(setting.insulinProductName)")
-                Text("단위 : \(setting.administration)")
+        Grid{
+            GridRow{
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("제품명 : \(setting.insulinProductName)")
+                        Text("단위 : \(setting.administration)")
+                    }
+                    Spacer()
+                }
+                .gridCellColumns(2)
+                
+                Button{
+                    
+                }label: {
+                    ZStack{
+                        Rectangle()
+                            .foregroundStyle(.mint)
+                        Text("제품 수 환산")
+                            .foregroundStyle(.black)
+                    }
+                }
+                .gridCellColumns(1)
             }
-            
-            Spacer()
-            
-            VStack{
+            GridRow{
+                Button{
+                    removeButtonTapped()
+                }label: {
+                    ZStack{
+                        Rectangle()
+                            .padding(.vertical, 1)
+                            .foregroundColor(.red)
+                            //.frame(maxWidth: 100)
+                        Text("삭제")
+                            .foregroundStyle(.white)
+                    }
+                }
+                .buttonStyle(BorderlessButtonStyle())
+                .gridCellColumns(1)
+                
                 Button{
                     //editButtonTapped(setting)
                     selectedInsulin = setting
@@ -32,25 +62,14 @@ struct InsulinSettingColumnView: View {
                         Rectangle()
                             .padding(.vertical, 1)
                             .foregroundColor(Color.gray)
-                            .frame(maxWidth: 100)
+                            //.frame(maxWidth: 100)
                         Text("편집")
                             .foregroundStyle(.white)
                     }
                 }
                 .buttonStyle(BorderlessButtonStyle())
-                Button{
-                    removeButtonTapped()
-                }label: {
-                    ZStack{
-                        Rectangle()
-                            .padding(.vertical, 1)
-                            .foregroundColor(.red)
-                            .frame(maxWidth: 100)
-                        Text("삭제")
-                            .foregroundStyle(.white)
-                    }
-                }
-                .buttonStyle(BorderlessButtonStyle())
+                .gridCellColumns(2)
+                
                 
             }
             

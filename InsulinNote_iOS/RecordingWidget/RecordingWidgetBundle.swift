17442074ad89e4fac98855e9041bc051fd96dc60
Heecheol_K <hck1104@naver.com>
2024-08-13 19:58:32 +0900

    위젯 프로젝트 추가

---
+++ InsulinNote_iOS/RecordingWidget/RecordingWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RecordingWidgetBundle.swift
+//  RecordingWidget
+//
+//  Created by 권희철 on 8/13/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RecordingWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RecordingWidget()
+    }
+}

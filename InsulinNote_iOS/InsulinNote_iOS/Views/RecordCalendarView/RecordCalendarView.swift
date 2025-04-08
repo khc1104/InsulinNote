95192fb6501fa75367e97c9da09f37cacefbb0a0
Heecheol_K <hck1104@naver.com>
2025-04-09 02:33:10 +0900

    년도와 월 값에 따라 해당 달력을 보여주는 뷰 만듬

---
+++ InsulinNote_iOS/InsulinNote_iOS/Views/RecordCalendarView/RecordCalendarView.swift
@@ -0,0 +1,85 @@
+//
+//  RecordCalendarView.swift
+//  InsulinNote_iOS
+//
+//  Created by 권희철 on 3/19/25.
+//
+
+import SwiftUI
+
+enum Weekday: Int, CaseIterable {
+    
+    case sunday = 1
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+    
+    func getString() -> String {
+        switch self {
+        case .sunday:
+            return "일"
+        case .monday:
+            return "월"
+        case .tuesday:
+            return "화"
+        case .wednesday:
+            return "수"
+        case .thursday:
+            return "목"
+        case .friday:
+            return "금"
+        case .saturday:
+            return "토"
+        }
+    }
+}
+
+struct RecordCalendarView: View {
+    let gridItems = Array(repeating: GridItem(.flexible()), count: 7)
+    @State var startDayOfWeek: Int = 0
+    @State var selectedYear: Int = 2025
+    @State var selectedMonth: Int = 4
+    var body: some View {
+        LazyVGrid(columns: gridItems) {
+            ForEach(Weekday.allCases, id: \.self){ dow in
+                Text("\(dow.getString())")
+            }
+            ForEach(1..<(startDayOfWeek + getLastDayOfMonth(selectedYear, selectedMonth)!), id: \.self){ item in
+                if item >= startDayOfWeek{
+                    Text("\(item - startDayOfWeek + 1)")
+                }else{
+                    Text("")
+                }
+            }
+        }.onAppear{
+            startDayOfWeek = getDayOfTheWeek(selectedYear, selectedMonth)
+        }
+    }
+    func getDayOfTheWeek(_ year:Int, _ month: Int) -> Int{
+        let calendar = Calendar.current
+        let components = DateComponents(year: year, month: month, day: 1)
+        if let date = calendar.date(from: components) {
+            let weekday = calendar.component(.weekday, from: date)
+            return weekday
+        }
+        return 0
+    }
+    func getLastDayOfMonth(_ year:Int, _ month: Int) -> Int?{
+        let calendar = Calendar.current
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let startDay = formatter.date(from: "\(year)-\(month)-01")!
+        let nextStartDay = calendar.date(byAdding: .month, value: 1, to: startDay)!
+        let end = calendar.dateComponents([.day], from: startDay, to:nextStartDay)
+        
+        guard let day = end.day else { return nil }
+        return day
+    }
+}
+
+#Preview {
+    RecordCalendarView()
+}

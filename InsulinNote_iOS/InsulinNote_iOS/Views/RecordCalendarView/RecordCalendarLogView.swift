9380fe815f635849376a68f304a6f7833e382762
Heecheol_K <hck1104@naver.com>
2025-05-09 02:04:52 +0900

    투여 기록 시트에 지효성 속효성 기록 칸을 나눠서 보여주게 수정

--- InsulinNote_iOS/InsulinNote_iOS/Views/RecordCalendarView/RecordCalendarLogView.swift
+++ InsulinNote_iOS/InsulinNote_iOS/Views/RecordCalendarView/RecordCalendarLogView.swift
@@ -14,16 +14,45 @@ struct RecordCalendarLogView: View {
     
     @Environment(\.modelContext) var insulinContext
     @Query var records: [InsulinRecordModel]
+    @Query var settings: [InsulinSettingModel]
     
+    @State var longActingInsulinSetting: InsulinSettingModel?
+    @State var fastActingInsulinSetting: InsulinSettingModel?
+    
+    @State var longActingInsulinLogs: [InsulinRecordModel] = []
+    @State var fastActingInsulinLogs: [InsulinRecordModel] = []
     var body: some View {
-        List {
-            ForEach(getInsulinLogsForActType(type: .long)){ log in
-                VStack{
-                    Text("이름: \(log.setting?.insulinProductName ?? "미지정")")
-                    Text("투여시간: \(log.createdAt.formatted(.dateTime))")
-                    Text("투여량: \(log.dosage)")
+        VStack(alignment: .leading){
+            Text("\(longActingInsulinSetting?.insulinProductName ?? "지효성 인슐린")")
+                .font(.title)
+                .padding(.top, 5)
+                .padding(.leading, 20)
+            VStack{
+                List{
+                    if let longActingInsulinLog = longActingInsulinLogs.first{
+                        VStack(alignment: .leading){
+                            Text("투여시간: \(longActingInsulinLog.createdAt.formatted(.dateTime))")
+                            Text("투여량: \(longActingInsulinLog.dosage)")
+                        }
+                    }
                 }
             }
+            Text("\(fastActingInsulinSetting?.insulinProductName ?? "속효성 인슐린")")
+                .font(.title)
+                .padding(.leading, 20)
+            List{
+                ForEach(fastActingInsulinLogs){ fastActingInsulinLog in
+                    VStack(alignment: .leading){
+                        Text("투여시간: \(fastActingInsulinLog.createdAt.formatted(.dateTime))")
+                        Text("투여량: \(fastActingInsulinLog.dosage)")
+                    }
+                }
+            }
+        }.onAppear{
+            longActingInsulinSetting = settings.first(where: {$0.actingType == .long})
+            fastActingInsulinSetting = settings.first(where: {$0.actingType == .fast})
+            longActingInsulinLogs = getInsulinLogsForActType(type: .long)
+            fastActingInsulinLogs = getInsulinLogsForActType(type: .fast)
         }
     }
     //인슐린 기록 불러오는 함수
@@ -32,7 +61,11 @@ struct RecordCalendarLogView: View {
         formatter.dateFormat = "yyyy-MM-dd"
         let filteredRecords = records.filter{
             let recordDate = formatter.string(from: $0.createdAt)
-            return recordDate == selectedDate
+            if recordDate == selectedDate && $0.setting?.actingType == type {
+                return true
+            }else{
+                return false
+            }
         }
         
         return filteredRecords
